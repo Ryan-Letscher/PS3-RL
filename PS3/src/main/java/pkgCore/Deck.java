52590964ee9dfb8c7dfd9b7975cd14ae481e8736
Ryan <Ryan@Ryan-Laptop>
2017-10-10 15:25:03 -0400

    yes

--- PS3/src/main/java/pkgCore/Deck.java
+++ PS3/src/main/java/pkgCore/Deck.java
@@ -25,13 +25,33 @@ public class Deck {
 	}
 	
 	//TODO: Write an overloaded Draw method to Draw a card of a given eSuit
+	public Card Draw(eSuit) {
+		return cardsInDeck;
+		
+	}
 	
 	//TODO: Write an overloaded Draw method to Draw a card of a given eRank
+	public Card Draw(eRank) {
+		return cardsInDeck;
+	}
 
 	//TODO: Write a method that will return the number of a given eSuit left in the deck.
+	public  Card count(eSuit) {
+		return cardsInDeck;
+	}
 	
 	//TODO: Write a method that will return the number of a given eRank left in the deck.
+	public Card count(eRank) {
+		return cardsInDeck;
+	}
 	
 	//TODO: Write a method that will return 0 or 1 if a given card is left in the deck.
+	public Card count(Card) {
+		if  (Card==cardsInDeck)
+			return 0;
+		else
+			return 1;
+	}
+	
 	
 }
